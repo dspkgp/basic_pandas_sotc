3614fd6e2931066a782deb86fa48e059026ae64d
Dheeraj Singh Patel <patel.drj@gmail.com>
2017-12-20 10:36:12 +0530

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -1,11 +1,12 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 style.use('ggplot')
 
-file_path = '/Users/dheerajpatel/Documents/spotmentor/SOTC_data/Book10.xlsx'
-df = pd.read_excel(file_path)
+filepath = os.getcwd() + '\Book10.xlsx'
+df = pd.read_excel(filepath)
 
 # Renaming different columns for future use
 df.rename(columns={'Total Exp':'Experience', 'Emp ID':'ID',
@@ -15,7 +16,7 @@ df.rename(columns={'Total Exp':'Experience', 'Emp ID':'ID',
 def cleanExp(experience):
     experience = experience.split()
     experience = map(lambda t : t.strip() , experience)
-    experience = filter(lambda t : t.isdigit(), experience)
+    experience = list(filter(lambda t : t.isdigit(), experience))
 
     return float(experience[0])+float(experience[1])/12
 
@@ -54,8 +55,6 @@ grade_count=[]
 appraiser_name=[]
 appraiser_count=[]
 
-import ipdb;ipdb.set_trace()
-
 def get_unique_name_and_count(column, uniquename, count):
 
 	for i in range(len(df[column].unique())):
@@ -78,72 +77,68 @@ plotting for different results
 and to get insights from the dataset
 """
 
-def data_visualization():
+#plot-1: plotting no of male and female employees
+f = df[df['Gender']=='Female']['ID'].count()
+m = df[df['Gender']=='Male']['ID'].count()
+df1 = pd.DataFrame([f,m])
+df1.index = ['Female','Male']
+df1.plot(kind='bar', stacked=True, label='Gender', figsize=(8,6))
+plt.ylabel('No of employee')
+plt.legend()
+plt.title('No. of male and female employees')
+plt.show()
+# plt.close()
 
-	#plot-1: plotting no of male and female employees
-	f = df[df['Gender']=='Female']['ID'].count()
-	m = df[df['Gender']=='Male']['ID'].count()
-	df1 = pd.DataFrame([f,m])
-	df1.index = ['Female','Male']
-	df1.plot(kind='bar', stacked=True, label='Gender', figsize=(8,6))
+
+#plot-2: plotting age distribution of employees
+figure = plt.figure(figsize=(8,6))
+bins = [10,20,30,40,40,50,60]
+plt.hist(df['Age'], bins, histtype='bar',color=['c'],label='Age', rwidth=0.8)
+plt.xlabel('Age')
+plt.ylabel('No of employee')
+plt.legend()
+plt.title('Age distribution of employees')
+plt.show()
+# plt.close()
+
+#plot-3: age distribution with gender
+figure = plt.figure(figsize=(8,6))
+plt.hist([df[df['Gender']=='Female']['Age'],df[df['Gender']=='Male']['Age']], stacked=True, color = ['g','r'],
+	bins = [10,20,30,40,40,50,60],label = ['Female','Male'])
+plt.xlabel('Age')
+plt.ylabel('Number of employee')
+plt.legend()
+plt.title('Age distribution with gender')
+plt.show()
+# plt.close()
+
+#plot-4: plotting distribution w.r.t experience
+figure = plt.figure(figsize=(8,6))
+bins = [0,2,4,6,8,10,12,14]
+plt.hist(df['Experience'], bins, histtype='bar',color=['y'],label='Age', rwidth=0.8)
+plt.xlabel('Experience (in years)')
+plt.ylabel('No of employee')
+plt.legend()
+plt.title('Distribution w.r.t experience')
+plt.show()
+# plt.close()
+
+def plotting_barplot(name,count,heading):
+	figure=plt.figure(figsize=(13,8))
+	x = [i for i in range(len(name))]
+	ax2 = plt.subplot2grid((1,1),(0,0))
+	ax2.bar(x,count,label='Grade',color='r')
+	plt.xticks(x,name,rotation=90)
+	plt.subplots_adjust(left=0.09,bottom=0.2,right=0.94,top=0.90,wspace=0.2, hspace=0)
+	plt.title('No. of employees w.r.t.' +  str(heading))
 	plt.ylabel('No of employee')
-	plt.legend()
-	plt.title('No. of male and female employees')
 	plt.show()
 	# plt.close()
 
-
-	#plot-2: plotting age distribution of employees
-	figure = plt.figure(figsize=(8,6))
-	bins = [10,20,30,40,40,50,60]
-	plt.hist(df['Age'], bins, histtype='bar',color=['c'],label='Age', rwidth=0.8)
-	plt.xlabel('Age')
-	plt.ylabel('No of employee')
-	plt.legend()
-	plt.title('Age distribution of employees')
-	plt.show()
-	# plt.close()
-
-	#plot-3: age distribution with gender
-	figure = plt.figure(figsize=(8,6))
-	plt.hist([df[df['Gender']=='Female']['Age'],df[df['Gender']=='Male']['Age']], stacked=True, color = ['g','r'],
-		bins = [10,20,30,40,40,50,60],label = ['Female','Male'])
-	plt.xlabel('Age')
-	plt.ylabel('Number of employee')
-	plt.legend()
-	plt.title('Age distribution with gender')
-	plt.show()
-	# plt.close()
-
-	#plot-4: plotting distribution w.r.t experience
-	figure = plt.figure(figsize=(8,6))
-	bins = [0,2,4,6,8,10,12,14]
-	plt.hist(df['Experience'], bins, histtype='bar',color=['y'],label='Age', rwidth=0.8)
-	plt.xlabel('Experience (in years)')
-	plt.ylabel('No of employee')
-	plt.legend()
-	plt.title('Distribution w.r.t experience')
-	plt.show()
-	# plt.close()
-
-	def plotting_barplot(name,count,heading):
-		figure=plt.figure(figsize=(13,8))
-		x = [i for i in range(len(name))]
-		ax2 = plt.subplot2grid((1,1),(0,0))
-		ax2.bar(x,count,label='Grade',color='r')
-		plt.xticks(x,name,rotation=90)
-		plt.subplots_adjust(left=0.09,bottom=0.2,right=0.94,top=0.90,wspace=0.2, hspace=0)
-		plt.title('No. of employees w.r.t.' +  str(heading))
-		plt.ylabel('No of employee')
-		plt.show()
-		# plt.close()
-
-	plotting_barplot(location_name,location_count,'location')
-	plotting_barplot(role_name,role_count,'role')
-	plotting_barplot(department_name,department_count,'department')
-	plotting_barplot(region_name,region_count,'region')
-	plotting_barplot(zone_name,zone_count,'zone')
-	plotting_barplot(grade_name,grade_count,'grades')
-	plotting_barplot(appraiser_name,appraiser_count,'appraiser')
-
-data_visualization()
+plotting_barplot(location_name,location_count,'location')
+plotting_barplot(role_name,role_count,'role')
+plotting_barplot(department_name,department_count,'department')
+plotting_barplot(region_name,region_count,'region')
+plotting_barplot(zone_name,zone_count,'zone')
+plotting_barplot(grade_name,grade_count,'grades')
+plotting_barplot(appraiser_name,appraiser_count,'appraiser')
